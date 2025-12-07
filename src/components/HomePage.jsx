3c0c82a910eec4f3818ab57e56f61f9503a4409e
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-12-07 12:22:44 +0530

    feat: add styles to HomePage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,71 +1,39 @@
 import { Suspense, useState } from "react";
 import {
-  useLoaderData,
   Await,
   Link,
-  useAsyncValue,
   useFetcher,
+  useLoaderData,
+  useAsyncValue,
 } from "react-router";
-
-function Post({ postData, type }) {
-  const { id, title, createdAt, updatedAt } = postData;
-
-  const fetcher = useFetcher();
-  const isDeleting = fetcher.state !== "idle";
-
-  const postUrl = `client link/${id}`;
-  const createTime = new Date(createdAt).toLocaleString();
-  const updateTime = new Date(updatedAt).toLocaleString();
-  const isUpdated = createTime !== updateTime;
-
-  return (
-    <div>
-      <h4>{type === "public" ? <Link to={postUrl}>{title}</Link> : title}</h4>
-      <p>Created At {createTime}</p>
-      {isUpdated && <p>Updated At {updateTime}</p>}
-      <div>
-        <Link to={`/posts/${id}/edit`}>Edit</Link>
-        <Link to={`/posts/${id}/comments`}>Comments</Link>
-        <fetcher.Form method="DELETE">
-          <button type="submit" name="postId" value={id} disabled={isDeleting}>
-            {isDeleting ? "DELETING" : "DELETE"}
-          </button>
-        </fetcher.Form>
-      </div>
-      <br />
-    </div>
-  );
-}
-
-function PrivatePosts() {
-  const resolvedPrivatePosts = useAsyncValue();
-  const { data: privatePostsArr } = resolvedPrivatePosts;
-  return (
-    <div>
-      {privatePostsArr.map((postData) => (
-        <Post key={postData.id} postData={postData} type="private" />
-      ))}
-    </div>
-  );
-}
+import styles from "../assets/css/HomePage.module.css";
 
 function HomePage() {
   const [activeSection, setActiveSection] = useState(0);
   const { publicPosts, privatePostsPromise } = useLoaderData();
-
   const { data: publicPostsArr } = publicPosts;
 
-  const fullName = localStorage.getItem("fullName");
-
   return (
-    <main>
-      <h2>Welcome to the Studio, {fullName}</h2>
+    <main className={styles.container}>
+      <p className={styles.title}>Posts</p>
 
-      <h3>Posts</h3>
-
-      <div>
-        <button onClick={() => setActiveSection(0)}>Public</button>
-        <button onClick={() => setActiveSection(1)}>Private</button>
+      <div className={styles.btnContainer}>
+        <button
+          className={`${styles.btn} ${
+            activeSection === 0 ? styles.activeBtn : ""
+          }`}
+          onClick={() => setActiveSection(0)}
+        >
+          Public
+        </button>
+        <button
+          className={`${styles.btn} ${
+            activeSection === 1 ? styles.activeBtn : ""
+          }`}
+          onClick={() => setActiveSection(1)}
+        >
+          Private
+        </button>
       </div>
 
       {activeSection === 0 && (
@@ -84,6 +52,62 @@ function HomePage() {
         </Suspense>
       )}
     </main>
+  );
+}
+
+function PrivatePosts() {
+  const resolvedPrivatePosts = useAsyncValue();
+  const { data: privatePostsArr } = resolvedPrivatePosts;
+  return (
+    <div>
+      {privatePostsArr.map((postData) => (
+        <Post key={postData.id} postData={postData} type="private" />
+      ))}
+    </div>
+  );
+}
+
+function Post({ postData, type }) {
+  const { id, title, createdAt, updatedAt } = postData;
+
+  const fetcher = useFetcher();
+  const isDeleting = fetcher.state !== "idle";
+
+  const postUrl = `client link/${id}`;
+  const createTime = new Date(createdAt).toLocaleString();
+  const updateTime = new Date(updatedAt).toLocaleString();
+  const isUpdated = createTime !== updateTime;
+
+  return (
+    <div className={styles.post}>
+      <h4 className={styles.postTitle}>
+        {type === "public" ? <Link to={postUrl}>{title}</Link> : title}
+      </h4>
+      <p className={styles.postInfo}>Created At {createTime}</p>
+      {isUpdated && <p className={styles.postInfo}>Updated At {updateTime}</p>}
+      <div>
+        <hr className={styles.separator} />
+        <div className={styles.postActionBar}>
+          <Link className={styles.actionLink} to={`/posts/${id}/comments`}>
+            Comments
+          </Link>
+          <Link className={styles.actionLink} to={`/posts/${id}/edit`}>
+            Edit
+          </Link>
+          <fetcher.Form method="DELETE" className={styles.dltForm}>
+            <button
+              className={styles.deleteBtn}
+              type="submit"
+              name="postId"
+              value={id}
+              disabled={isDeleting}
+            >
+              {isDeleting ? "Deleting" : "Delete"}
+            </button>
+          </fetcher.Form>
+        </div>
+      </div>
+    </div>
   );
 }
 
